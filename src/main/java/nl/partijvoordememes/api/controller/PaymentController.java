e84e024e5552e0bcb481a1333c2494bdd53c92bb
Michel Bijnen <michelbijnenmichelbijnen@gmail.com>
2020-12-08 01:12:40 +0100

    feat: added documentation tool

--- src/main/java/nl/partijvoordememes/api/controller/PaymentController.java
+++ src/main/java/nl/partijvoordememes/api/controller/PaymentController.java
@@ -2,6 +2,6 @@ package nl.partijvoordememes.api.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/payment")
 public class PaymentController {
 }
