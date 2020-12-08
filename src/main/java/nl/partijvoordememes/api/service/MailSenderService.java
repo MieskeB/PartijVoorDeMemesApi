46ab9b2a97b25984955df243ebd118527af0a0c5
Michel Bijnen <michelbijnenmichelbijnen@gmail.com>
2020-12-08 01:42:49 +0100

    feat: made send email working

--- src/main/java/nl/partijvoordememes/api/service/MailSenderService.java
+++ src/main/java/nl/partijvoordememes/api/service/MailSenderService.java
@@ -1,13 +1,9 @@
 package nl.partijvoordememes.api.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
-
-import java.util.Properties;
 
 @Service
 public class MailSenderService {
@@ -22,23 +18,5 @@ public class MailSenderService {
         message.setSubject(subject);
         message.setText(text);
         this.emailSender.send(message);
-    }
-
-    @Bean
-    public JavaMailSender getJavaMailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.partijvoordememes.nl");
-        mailSender.setPort(587);
-
-        mailSender.setUsername(System.getProperty("EMAIL_USERNAME"));
-        mailSender.setPassword(System.getProperty("EMAIL_PASSWORD"));
-
-        Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true");
-
-        return mailSender;
     }
 }
